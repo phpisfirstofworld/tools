37ebe31d495eca670399b8572886fa63851d67a2
杨洋 <you@example.com>
2021-02-22 16:31:21 +0800

    优化文件摆放

--- time.go
+++ time.go
@@ -1,30 +1,11 @@
 package tools
 
 import (
-	"crypto/md5"
-	"fmt"
 	"github.com/PeterYangs/tools/com"
-	"io"
 	"regexp"
 	"strings"
 	"time"
 )
-
-//php中的常用函数，go重写
-
-func Explode(delimiter, text string) []string {
-	if len(delimiter) > len(text) {
-		return strings.Split(delimiter, text)
-	} else {
-		return strings.Split(text, delimiter)
-	}
-}
-
-func Md5(str string) string {
-	h := md5.New()
-	io.WriteString(h, str)
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
 
 //先用比较蠢的办法
 func Date(format string, timestamp int64) string {
