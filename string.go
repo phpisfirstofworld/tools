691f8b8978f9da66010343e2cc0067655b5c2406
杨洋 <you@example.com>
2021-02-22 16:31:05 +0800

    优化文件摆放

---
+++ string.go
@@ -0,0 +1,22 @@
+package tools
+
+import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func Explode(delimiter, text string) []string {
+	if len(delimiter) > len(text) {
+		return strings.Split(delimiter, text)
+	} else {
+		return strings.Split(text, delimiter)
+	}
+}
+
+func Md5(str string) string {
+	h := md5.New()
+	io.WriteString(h, str)
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
